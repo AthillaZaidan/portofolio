425e03194e85190642ef7fe013e1d54b8603e436
AthillaZaidan <athillazaidanstudy@gmail.com>
2026-05-01 14:22:17 +0700

    feat: add hooks and utilities

---
+++ src/lib/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Azeret_Mono } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+export const azeretMono = Azeret_Mono({
+  subsets: ["latin"],
+  variable: "--font-azeret-mono",
+  display: "swap",
+});
