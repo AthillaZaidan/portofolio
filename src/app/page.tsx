533cb890ef73ae04c5cf1d0696dc8dfd8507b062
AthillaZaidan <athillazaidanstudy@gmail.com>
2026-07-07 13:15:08 +0700

    feat(portfolio): route home to brand page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,5 @@
-import { Navbar } from "@/components/Navbar";
-import { Hero } from "@/components/Hero";
-import { About } from "@/components/About";
-import { Skills } from "@/components/Skills";
-import { ProjectCarousel } from "@/components/ProjectCarousel";
-import { Experience } from "@/components/Experience";
-import { Contact } from "@/components/Contact";
-import { Footer } from "@/components/Footer";
+import { PortfolioBrandPage } from "@/components/brand/PortfolioBrandPage";
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <main>
-        <Hero />
-        <About />
-        <Skills />
-        <ProjectCarousel />
-        <Experience />
-        <Contact />
-      </main>
-      <Footer />
-    </>
-  );
+  return <PortfolioBrandPage />;
 }
